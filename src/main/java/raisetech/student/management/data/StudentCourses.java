1e8e44f1a1ca010c0e054d8f7538088cc833d7f3
Seigo <seigo@ombp2025.local>
2025-09-01 22:57:09 +0900

    thymeleafを使ったReadの画面描画処理を実装 studentListのhtmlファイルを新たに作成

---
+++ src/main/java/raisetech/student/management/data/StudentCourses.java
@@ -0,0 +1,16 @@
+package raisetech.student.management.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentCourses {
+
+  private String id;
+  private String studentId;
+  private String courseName;
+  private String startDate;
+  private String endDate;
+
+}
